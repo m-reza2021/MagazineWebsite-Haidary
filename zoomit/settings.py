1529143ad3e2f454ee6588220ca60c66eafc0172
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2021-06-28 14:05:41 +0430

    django-on-heroku

--- zoomit/settings.py
+++ zoomit/settings.py
@@ -156,9 +156,9 @@ USE_TZ = True
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
 
 MEDIA_URL = '/media/'
 
