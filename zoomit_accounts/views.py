a4acaac78d129fef4f6f0394026946034cc93d13
Ali Ahmad Ataie Hazara <iamaliataie@gmail.com>
2021-06-28 14:30:52 +0430

    django-on-heroku

--- zoomit_accounts/views.py
+++ zoomit_accounts/views.py
@@ -95,7 +95,7 @@ def profile(request):
             x.save()
 
     context = {
-        'page_title': profile.user.get_full_name(),
+        'page_title': profile.user,
         'image': profile,
         'image_form': image_form,
         'profile_form': profile_update_form,
